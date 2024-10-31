391d50564ba8717e18abc64ae508c5ef3d1711f8
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-10-31 12:50:24 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-# myproject/myproject/settings.py
+
 import os
 
 # Media files settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This is where uploaded files will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
